4d1bc3202bb4eca2e15e21d2be6510d5d8df2a0c
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-10-23 16:20:32 -0300

    Config Typescript, Prisma and Github OAuth

---
+++ node-api/src/app.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express, { Request, response, Response } from 'express';
+
+const app = express();
+
+console.log(process.env.GITHUB_CLIENT_ID);
+
+app.get('/', (request: Request, response: Response) =>
+  response.json({ status: true, message: 'Hello World' })
+);
+
+app.get('/github', (request: Request, response: Response) =>
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
+);
+
+app.get('/signin/callback', (request: Request, response: Response) => {
+  const { code } = request.query;
+  response.json({ code });
+});
+
+app.listen(4000, () => console.log('Server listen on port 4000'));
