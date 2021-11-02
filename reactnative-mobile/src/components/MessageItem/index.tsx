6bb7331e594507a98ec7ac68963a517cbfa1abb6
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-11-01 22:27:02 -0300

    RN - Adding moti animations

--- reactnative-mobile/src/components/MessageItem/index.tsx
+++ reactnative-mobile/src/components/MessageItem/index.tsx
@@ -1,3 +1,4 @@
+import { MotiView } from "moti";
 import React from "react";
 
 import { Text, View } from "react-native";
@@ -12,12 +13,17 @@ type MessageItemProps = {
 
 export function MessageItem({ data }: MessageItemProps) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      style={styles.container}
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: "timing", duration: 700 }}
+    >
       <Text style={styles.text}>{data.text}</Text>
       <View style={styles.userInfo}>
         <UserPhoto size="SMALL" imageUri={data.user.avatar_url} />
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
